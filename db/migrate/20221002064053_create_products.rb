6b0b9b687acd240959c07c11b5a9459eedddac04
FB <fabricebaron@hotmail.com>
2022-10-02 11:32:12 +0200

    Ecommerce 1

---
+++ db/migrate/20221002064053_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.integer :quantity, default: 1
+      t.references :merchant, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
